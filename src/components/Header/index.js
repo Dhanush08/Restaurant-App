e472937230a28ddc5bffcab62a645c645220dfea
Dhanush08 <dhanushkondaparthi1289@gmail.com>
2024-03-12 12:32:31 +0000

    Additional Features

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,38 +1,54 @@
+import {useContext} from 'react'
+import {Link, withRouter} from 'react-router-dom'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import Cookies from 'js-cookie'
+
+import CartContext from '../../context/CartContext'
+
 import './index.css'
 
-const Header = ({cartItems}) => {
-  const getCartItemsCount = () =>
-    cartItems.reduce((acc, item) => acc + item.quantity, 0)
+const Header = props => {
+  const {cartList, restaurantName} = useContext(CartContext)
+
+  const onLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
 
   const renderCartIcon = () => (
-    <div className="cart-icon-container">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="35"
-        height="35"
-        fill="#585555"
-        className="bi bi-cart3 cart-icon"
-        viewBox="0 0 16 16"
-      >
-        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-      </svg>
+    <div className="cart-icon-link">
+      <Link to="/cart">
+        <button type="button" className="cart-icon-button" data-testid="cart">
+          <AiOutlineShoppingCart className="cart-icon" />
+        </button>
+      </Link>
       <div className="cart-count-badge d-flex justify-content-center align-items-center">
-        <p className="m-0 cart-count">{getCartItemsCount()}</p>
+        <p className="m-0 cart-count">{cartList.length}</p>
       </div>
     </div>
   )
 
   return (
-    <header className="p-4 d-flex flex-row align-items-center justify-content-between nav-header">
-      <h1 className="m-0 logo-heading">Shine Cafe</h1>
+    <header className="p-4 d-flex flex-row align-items-center nav-header">
+      <Link to="/">
+        <h1 className="m-0 logo-heading">{restaurantName}</h1>
+      </Link>
       <div className="d-flex flex-row align-items-center ms-auto">
-        <p className="mt-0 mb-0 me-2 mr-4 d-none d-sm-block my-orders-text">
+        <p className="mt-0 mb-0 me-2 d-none d-sm-block my-orders-text">
           My Orders
         </p>
+        <button
+          type="button"
+          className="btn btn-outline-danger ms-2 me-2 btn-sm"
+          onClick={onLogout}
+        >
+          Logout
+        </button>
         {renderCartIcon()}
       </div>
     </header>
   )
 }
 
-export default Header
+export default withRouter(Header)
